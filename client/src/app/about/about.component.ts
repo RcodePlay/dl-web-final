6980cf84f0cbc2c56257e339a4d4a60196deb995
rcodeplay <ryancode.eh@gmail.com>
2024-09-09 17:02:45 +0200

    Changed how about component works

--- client/src/app/about/about.component.ts
+++ client/src/app/about/about.component.ts
@@ -1,24 +1,47 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import {
-    RouterLink,
-    RouterLinkActive,
-    RouterModule,
-    RouterOutlet,
-} from '@angular/router';
 @Component({
     selector: 'app-about',
     standalone: true,
-    imports: [
-        CommonModule,
-        RouterLink,
-        RouterLinkActive,
-        RouterOutlet,
-        RouterModule,
-    ],
+    imports: [CommonModule],
     templateUrl: './about.component.html',
     styleUrl: './about.component.scss',
 })
 export class AboutComponent {
-    // it might be empty here, but it's doing it's job in the html file
+    activeDivision: number | null = null;
+
+    divisions = [
+        {
+            name: 'Zbor',
+            leaders: 'Pavol Marcincin',
+            dateCreated: '2024',
+            description: '',
+        },
+        {
+            name: 'Division 1',
+            leaders: '',
+            dateCreated: '2024',
+            description: '',
+        },
+        {
+            name: 'Division 2',
+            leaders: '',
+            dateCreated: '2024',
+            description: '',
+        },
+        {
+            name: 'Division 3',
+            leaders: '',
+            dateCreated: '2024',
+            description: '',
+        },
+    ];
+
+    toggleDivision(index: number) {
+        if (this.activeDivision === index) {
+            this.activeDivision = null; // Close the currently open division
+        } else {
+            this.activeDivision = index; // Open the selected division
+        }
+    }
 }
--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,10 +3,6 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { EventsComponent } from './events/events.component';
 import { SupportUsComponent } from './support-us/support-us.component';
-import { Oddiel1Component } from './about/oddiel1/oddiel1.component';
-import { Oddiel2Component } from './about/oddiel2/oddiel2.component';
-import { Oddiel3Component } from './about/oddiel3/oddiel3.component';
-import { ZborComponent } from './about/zbor/zbor.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent, title: '4. zbor - Domov' },
@@ -18,16 +14,5 @@ export const routes: Routes = [
     },
     { path: '', redirectTo: '/home', pathMatch: 'full' },
 
-    {
-        path: 'about',
-        component: AboutComponent,
-        title: '4. zbor - O nás',
-        children: [
-            { path: 'zbor', component: ZborComponent },
-            { path: 'oddiel1', component: Oddiel1Component },
-            { path: 'oddiel2', component: Oddiel2Component },
-            { path: 'oddiel3', component: Oddiel3Component },
-            { path: 'about', redirectTo: '/zbor', pathMatch: 'full' },
-        ],
-    },
+    { path: 'about', component: AboutComponent, title: '4. zbor - O nás' },
 ];
