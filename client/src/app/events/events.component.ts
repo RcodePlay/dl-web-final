410812b6a5a91c9db4963c598941301e9843045e
rcodeplay <ryancode.eh@gmail.com>
2024-09-12 17:08:48 +0200

    Finished event component

--- client/src/app/events/events.component.ts
+++ client/src/app/events/events.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, HostListener } from '@angular/core';
 import { EventService } from '../event.service';
 @Component({
     selector: 'app-events',
@@ -9,15 +9,40 @@ import { EventService } from '../event.service';
     styleUrl: './events.component.scss',
 })
 export class EventsComponent implements OnInit {
+    selectedEvent: any | null = null;
+    events: any = [];
+
     // I've never before seen someone use @Inject() for using a service in a component
     constructor(@Inject(EventService) private event: EventService) {}
 
-    events: any = [];
-
     ngOnInit(): void {
-        this.event.getEvents().subscribe((recievedEvents: any) => {
-            this.events = recievedEvents;
+        this.event.getEvents().subscribe((data: any) => {
+            this.events = data;
         });
         console.log(this.events);
+    }
+
+    openEventDetails(event: any) {
+        this.selectedEvent = event;
+    }
+
+    closeEventDetails() {
+        this.selectedEvent = null;
+    }
+
+    @HostListener('document:click', ['$event'])
+    onClickOutside(event: MouseEvent) {
+        const target = event.target as HTMLElement;
+        if (
+            this.selectedEvent &&
+            !target.closest('.event-details') &&
+            !target.closest('.event-item')
+        ) {
+            this.closeEventDetails();
+        }
+    }
+
+    onEventDetailsClick(event: MouseEvent) {
+        event.stopPropagation();
     }
 }
