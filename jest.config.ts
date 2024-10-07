b1d37ec5522553fa3705d450829c51fdf168fd6a
rcodeplay <ryancode.eh@gmail.com>
2024-10-07 20:19:14 +0200

    Config files update

--- jest.config.ts
+++ jest.config.ts
@@ -3,9 +3,13 @@ import type { Config } from '@jest/types';
 const config: Config.InitialOptions = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
+    testMatch: ['**/?(*.)+(test).[jt]s?(x)'],
     moduleFileExtensions: ['ts', 'tsx', 'js'],
     silent: true,
+    transform: {
+        '^.+\\.ts$': 'ts-jest',
+    },
+    extensionsToTreatAsEsm: ['.ts'],
 };
 
 export default config;
